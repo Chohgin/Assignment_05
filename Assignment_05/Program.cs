70da2dc9c889453371f1f8e529b6dd024e2d12ee
Chohgin <nickwalker867@gmail.com>
2015-03-25 17:27:46 -0400

    Set up switch cases and began creating user input

--- Assignment_05/Program.cs
+++ Assignment_05/Program.cs
@@ -1,4 +1,7 @@
-﻿/*
+﻿/*Assignent 5 - IO and Exception Handling
+ *Nicholas Walker
+ *25/03/2015
+ *Program that stores data from a file and displays it to the user.
  */
 using System;
 using System.Collections.Generic;
@@ -18,7 +21,7 @@ namespace Assignment_05
             int choice = 0;
 
             //menu
-            while (choice != 2)
+            while (choice != 3)
             {
 
                 Console.WriteLine("+++++++++++++++++++++++++++++++++++++");
@@ -26,31 +29,56 @@ namespace Assignment_05
                 Console.WriteLine("1. Display Grades");
                 Console.WriteLine("2. Exit Program");
                 Console.WriteLine("+++++++++++++++++++++++++++++++++++++");
-                switch (choice)
-                {
-                    case 1:
-                        {
-                            break;
-                        }
-                    case 2:
-                        {
-                            break;
-                        }
+                choice = Convert.ToInt32(Console.ReadLine());
+                
+                    switch (choice)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Please enter the name of your file:");
+                                input = Console.ReadLine();
+                                try
+                                {
+                                    // code found at http://www.dotnetperls.com/file
+                                    using (StreamReader read = new StreamReader(input))
+                                    {
+                                        string line;
+                                        while ((line = read.ReadLine()) != null)
+                                        {
+                                            string[] student = line.Split(',');
+                                        }
+                                    }
+                                }
+                                catch(Exception error)
+                                {
+                                    Console.WriteLine("File does not exist. Please try again");
+                                    Console.WriteLine("Please press any key to continue");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    choice = 0;
+                                }
+                                Console.WriteLine("+++++++++++++++++++++++++++++++++++++");
+                                Console.WriteLine();
+                                Console.WriteLine("Please press any key to continue");
+                                Console.ReadKey();
+                                Console.Clear();
+                                choice = 0;
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Thank you, have a nice day");
+                                Console.WriteLine("Press any key to conintue");
+                                Console.ReadKey();
+                                choice = 3;
+                                break;
+                            }
 
-                }
+                    }
+               
             }
 
-            // code found at http://www.dotnetperls.com/file
-            using (StreamReader read = new StreamReader("StudentGrades.txt"))
-            {
-                string line;
-                while ((line = read.ReadLine()) != null)
-                {
-                    string[] student = line.Split(',');
-                }
-
-
-            }
+           
         }
     }
 }
