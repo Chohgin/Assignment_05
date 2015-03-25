b8573d76645340ba8480ab8cfa474ab399dd69e2
Chohgin <nickwalker867@gmail.com>
2015-03-25 17:12:18 -0400

    Set up menu, found help with reading the file, setting up switch statment

--- Assignment_05/Program.cs
+++ Assignment_05/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿/*
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace Assignment_05
 {
@@ -10,6 +14,43 @@ namespace Assignment_05
     {
         static void Main(string[] args)
         {
+            string input;
+            int choice = 0;
+
+            //menu
+            while (choice != 2)
+            {
+
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++");
+                Console.WriteLine("Please choose an option below 1 or 2:");
+                Console.WriteLine("1. Display Grades");
+                Console.WriteLine("2. Exit Program");
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++");
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+
+                }
+            }
+
+            // code found at http://www.dotnetperls.com/file
+            using (StreamReader read = new StreamReader("StudentGrades.txt"))
+            {
+                string line;
+                while ((line = read.ReadLine()) != null)
+                {
+                    string[] student = line.Split(',');
+                }
+
+
+            }
         }
     }
 }
